42aca7b874584eca8731a1e4a12d6de43c3c045d
Niklas Jansson <Niklas.Jansson91@hotmail.com>
2018-01-18 11:37:19 +0100

    Testar ladda JSON

---
+++ www/js/app.js
@@ -0,0 +1,15 @@
+class App{
+	constructor(){
+		JSON._classes(Movie);
+		this.movie = {};
+		JSON._load('film.json').then(movie=>{
+  		this.movie = movie;
+  		console.log(this.movie);
+  		this.movie.forEach(thing=>{
+  			console.log(thing.title);
+  		})
+		});
+	}
+}
+
+const app = new App();
