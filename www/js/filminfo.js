00c48b0e85c60470d6638c1f83143edc8b2e187a
Aiko M <minmilera@gmail.com>
2018-01-18 23:35:13 +0100

    Skapat FilmInfo klass

---
+++ www/js/filminfo.js
@@ -0,0 +1,27 @@
+class FilmInfo extends Film {
+
+	// ?? 引数名がtitleかは、未明
+	// constructor(title) {
+	// 	super(title);
+	// }
+
+	
+
+
+	// renderFilmInfo() {
+	// 	$('main #main-modal').remove();
+	// 	$('main').append(`
+		
+		
+		
+		
+		
+		
+		
+	// 	`)
+
+	// }
+
+
+
+}
