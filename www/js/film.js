bb600dedda9961637ecf17cd9585d22e5c4fd898
Niklas Jansson <Niklas.Jansson91@hotmail.com>
2018-01-19 18:26:04 +0100

    La till function för att få popover att fungera.

--- www/js/film.js
+++ www/js/film.js
@@ -6,33 +6,16 @@ class Movie{
 		// this.name = jsonFilmer[urlName].title;
 		// this.name = jsonFilmer[urlName].releaseYear;
 		// this.name = jsonFilmer[urlName].runtime;
-
-		// this.title = title;
-		// this.productionYear = productionYear;
-		// this.length = length;
-		// this.genre = genre;
-		// this.language = language;
-		// this.director = director;
-		// this.actors = actors;
-		// this.description = description;
-		// this.images = images;
-		// this.youtubeTrailers = youtubeTrailers;
 	}
-
-
-
 
 	load(data){
 		console.log(data);
 		this.number = data.numberOfMovies;
 	}
-
 	getData(){
 	}
-
-
-
-
-
-
 }
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
