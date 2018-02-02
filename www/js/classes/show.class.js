12a61b3fd77ddb5bbdf407e0f74517910e0fc392
Unknown <eyad.al@hotmail.se>
2018-02-02 13:29:50 +0100

    justerat show class

--- www/js/classes/show.class.js
+++ www/js/classes/show.class.js
@@ -1,8 +1,10 @@
-class Show {
+class Show extends Base {
   
   constructor(props) {
     // Have to comment out the constructor for JSON Flex to load from here
+    super();
     for (let name in props){
+    	if(['date','film','auditorium','time'].includes(name)){
       this[name] = props[name];
     }
     // this.auditorium = data.auditorium;
@@ -11,4 +13,5 @@ class Show {
     // this.time = data.time;
 
   }
+ }
 }
