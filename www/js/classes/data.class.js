d81de11fcdccd4bd7bda189d437e17b9b810f19b
Aiko M <minmilera@gmail.com>
2018-02-05 00:03:48 +0100

    Lagt till class namn till booking.json

--- www/js/classes/data.class.js
+++ www/js/classes/data.class.js
@@ -4,7 +4,7 @@ class Data{
 		this.load();
 	}
 	async load(){
-		JSON._classes(Movie, UpcomingMovie, Show, User);
+		JSON._classes(Movie, UpcomingMovie, Show, User, BookingData);
 		Data.movies = await JSON._load("film");　　// 7. film = film.json, もしjsonファイルを増やしたらここに追加
 		Data.upcoming = await JSON._load("premiar");
     Data.shows = await JSON._load("shows");
