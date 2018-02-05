8d65b4d70df244443c582d2a65b5b18de51b5743
Unknown <eyad.al@hotmail.se>
2018-02-05 09:08:43 +0100

    loop på kalendarium class

--- www/js/classes/kalendarium.class.js
+++ www/js/classes/kalendarium.class.js
@@ -3,11 +3,20 @@ class Kalendarium extends Base {
 	constructor(){
 		super();
 
-		this.blocks = [];
+		this.days = [];
+		let array = [];
 		for(let show of Data.shows){
-				this.blocks.push(new Show(show))
-		}	
+			let day = new Date(show.date).getDate();
+			let month = new Date(show.date).getMonth() + 1;
+			if (  ) {
+				array.push(show);
+			} else {
+				this.days.push({ [show.date]: array});
+			}
 
-	}
+		}	// /loop
+		console.log(this.days);
+	} // /constructor
 
-}
+} // /class
+
