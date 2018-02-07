dada27c89dab1b850abfecc359986361bd016ac7
Unknown <eyad.al@hotmail.se>
2018-02-07 11:48:08 +0100

    day class visar datum

--- www/js/classes/day.class.js
+++ www/js/classes/day.class.js
@@ -3,17 +3,9 @@ class Day extends Base {
   constructor(props) {
     // Have to comment out the constructor for JSON Flex to load from here
     super();
-    let day = [];
-      for(let show of shows)
-        if(day && day.date == show.date){
-          day.shows.push(show);
-        }
-        else{
-          if(!day){
-            let day ={shows:[]};
-          }
-          day.shows.push(show);
-        }
-        show.date
+    console.log(props);
+    this.shows = props.shows;
+    this.date = this.shows[0].date;
+
   }
 }
